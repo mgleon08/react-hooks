68ecfeaef00cf45377e9cb700ab3ce2f1ea9d45a
Leon Ji <mgleon08@gmail.com>
2019-09-28 23:06:15 +0800

    The useReducer Hook

--- src/index.js
+++ src/index.js
@@ -1,27 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import { start } from 'pretty-error';
+
+const notesReducer = (state, action) => {
+  switch (action.type) {
+    case 'POPULATE_NOTES':
+      return action.notes;
+    case 'ADD_NOTE':
+      debugger;
+      return [...state, { title: action.title, body: action.body }];
+    case 'REMOVE_NOTE':
+      return state.filter(note => note.title !== action.title);
+    default:
+      return state;
+  }
+};
 
 const NoteApp = () => {
-  const [notes, setNotes] = useState([]);
+  // const [notes, setNotes] = useState([]);
+  const [notes, dispatch] = useReducer(notesReducer, []);
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
 
   const addNote = e => {
     e.preventDefault();
-    setNotes([...notes, { title, body }]);
+    // setNotes([...notes, { title, body }]);
+    dispatch({
+      type: 'ADD_NOTE',
+      title,
+      body,
+    });
     setTitle('');
     setBody('');
   };
 
   const removeNote = title => {
-    setNotes(notes.filter(note => note.title !== title));
+    // setNotes(notes.filter(note => note.title !== title));
+    dispatch({
+      type: 'REMOVE_NOTE',
+      title,
+    });
   };
 
   useEffect(() => {
-    const notesData = JSON.parse(localStorage.getItem('notes'));
-    if (notesData) {
-      setNotes(notesData);
+    const notes = JSON.parse(localStorage.getItem('notes'));
+    if (notes) {
+      // setNotes(notesData);
+      dispatch({ type: 'POPULATE_NOTES', notes });
     }
   }, []);
 
